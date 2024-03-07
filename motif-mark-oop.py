6723db9ff1bfae011ced647f457b8e414172853e
bisetegn <bisetegn@uoregon.edu>
2024-03-06 18:28:04 -0800

    upload

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -17,7 +17,7 @@ motif=args.motiffilename
 
 fasta = {}
 
-def oneline_fasta(filename: str) -> dict:
+def oneline_fasta(filename: str) -> dict[str, str]:
     '''Reads a fasta file and returns a dictionary with header as keys and sequences as values'''
 
     with open(fasta_file, "r") as fh:
@@ -38,48 +38,124 @@ def oneline_fasta(filename: str) -> dict:
 
     return fasta
 
-#3 classes -class gene - store gene name, class seq- stors gene length, class motif- identifies motif positions
-# class for drawing  
+# create a dictionary for motifs as key-motifs in file, values - identified motifs
+
+motifs={}
+# 
+#key- motifs in file
+#value - regex expression
+
+def motif_to_regex(motif: str) -> str:
+    #ex- motif = ycgy --> "[CT]CG[CT]"
+    motif_regex = ''
+    for base in motif:
+        if base.upper() == "A":
+            motif_regex += "A"
+        elif base.upper() == "C":
+            motif_regex += "C"
+        elif base.upper() == "G":
+            motif_regex += "G"
+        elif base.upper() == "T":
+            motif_regex += "T"
+        elif base.upper() == "U":
+            motif_regex += "U"
+        elif base.upper() == "R":
+            motif_regex += "[AG]"
+        elif base.upper() == "Y":
+            motif_regex += '[CT]'
+        elif base.upper() == "S":
+            motif_regex += "[GC]"
+        elif base.upper() == "W":
+            motif_regex += "[AT]"
+        elif base.upper() == "K":
+            motif_regex += "[GT]"
+        elif base.upper() == "M":
+            motif_regex += "[AC]"
+        elif base.upper() == "B":
+            motif_regex += "[CGT]"
+        elif base.upper() == "D":
+            motif_regex += "[AGT]"
+        elif base.upper() == "H":
+            motif_regex += "[ACT]"
+        elif base.upper() == "V":
+            motif_regex += "[ACG]"
+        elif base.upper() == "N":
+            motif_regex += "[CTGAU]"
+    return motif_regex
+
+#print(motif_to_regex("ycgy"))
+
+with open ("Fig_1_motifs.txt", "r") as file:
+    for motif in file:
+        my_motif= motif.strip().upper()
+        #setting motif- ygcy set as key of motifs dictionary
+        motifs[my_motif]= motif_to_regex(my_motif)
+
+#with open("test.fa", "r") as fh2:
+    #seq- aaaactcgaaa
+for sequence in fasta.items(): 
+    matches = re.finditer( motif_to_regex("catag"), sequence, re.IGNORECASE)
+    for match in matches:
+        print("-----")
+        print(match)
+        #print(type(match))
+        #print(dir(match))
+        # print(match.start())
+        # print(match.end())
+        print(match.span())
+
+"""
+dictionary with a key - motifs in file ex- 'YgcY' 
+with value - regex expression of that motif EX- '[CT]gc[CT]
+
+"""
+
+#3 classes -class gene - store gene name, 
+    #class seq- stors gene length, 
+    #groups for saving information
+
 class Gene:
-    def __init__(self, the_name, the_header):
+    def __init__(self, the_name: str, the_number: int, the_length: int):
         # Initialize instance attributes
-        self.name = the_name
-        self.header = the_header
-        # You can initialize other attributes here
-    def get_gene(self):
-        return self.name
-
-class Sequence:
-    def __init__(self, the_name, the_sequence, the_length, the_motif):
-        # Initialize instance attributes
-        self.name = the_name
-        self.sequence = the_sequence
+        self.name = the_name 
+        self.number = the_number
         self.length = the_length
-        self.motif = the_motif
-        # You can initialize other attributes here
-    def get_lenseq(self):
-        return self.length
     
-    def get_motif(self):
-        return self.motif
+    def __repr__(self) -> str:
+        return f"Gene({self.name}, {self.number}, {self.length})"
     
-    for key, value in fasta.items():# parse through the value and 
-        matches = re.finditer(motif, value, re.IGNORECASE)
+    #def draw(self, the_context: what_type??):
+    def draw(self, context):
+        # Set color and line width for drawing gene
+        context.set_source_rgb(0, 0, 0)  # Black color
+        context.set_line_width(5)
 
-    
-class Drawing:
-    def __init__(self, width= 2000.6, height = 200):
-        self.width = width
-        self.height = height
-        self.surface = cairo.PDFSurface("motif_mark.png", width, height)
-        self.context = cairo.Context(self.surface)
-        
-    def draw_line(self, x1, y1, x2, y2, line_width=1):
-        self.context.set_line_width(line_width)
-        self.context.move_to(x1, y1)
-        self.context.line_to(x2, y2)
-        self.context.stroke()
-        
-    def draw_rectangle(self, x, y, width, height):
-        self.context.rectangle(x, y, width, height)
-        self.context.fill()
+        # Calculate start and end positions for drawing the gene line
+        start_x = self.number * 100
+        end_x = start_x + self.length
+
+        # Draw gene line
+        context.move_to(start_x, 50)
+        context.line_to(end_x, 50)
+        context.stroke()
+
+        # Draw gene label
+        context.move_to(start_x, 30)
+        context.show_text(self.name)
+
+# Create instances of Gene
+g1 = Gene("INSR", 1, 24)
+g2 = Gene("MNSB", 2, 21)
+
+# Create an SVG surface and draw genes on it
+with cairo.SVGSurface("genes.svg", 500, 100) as surface:
+    context = cairo.Context(surface)
+    context.set_source_rgb(1, 1, 1)  # White background
+    context.paint()
+
+    # Draw genes
+    g1.draw(context)
+    g2.draw(context)
+
+    surface.write_to_png('gene3.png')
+
